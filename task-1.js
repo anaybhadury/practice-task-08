329c95660a5886c3f70c4df2bb1af46d16bade8c
anaybhadury <bhadury9292@gmail.com>
2024-09-07 21:03:17 +0600

    added practice task 1

--- task-1.js
+++ task-1.js
@@ -1,3 +1,10 @@
 /**
  * WRITE A FUNCTION TO CONVERT TEMPERATURE FROM CELSIUS TO FARENHEIT.
  */
+
+function celsToFaren(celsius){
+    return (celsius * (9 / 5)) + 32;
+}
+const tempCelsius = 1;
+const tempFarenheit = celsToFaren(tempCelsius);
+console.log(tempFarenheit);
