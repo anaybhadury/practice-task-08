b6099c3fbfa5b3929fc6fdbf8f326df30eea9126
anaybhadury <bhadury9292@gmail.com>
2024-09-09 12:00:23 +0600

    added practice task 3

--- task-3.js
+++ task-3.js
@@ -2,3 +2,55 @@
  * WRITE A FUNCTION TO COUNT THE NUMBER OF VOWELS IN A STRING.
  */
 
+function stringFind (str){
+    let smallChar = str.toLowerCase();
+
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    let count = 0;
+
+    for(char of smallChar){
+        if(vowels.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
+let result = stringFind('Bangaldesh');
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getString(string){
+//     let lowerCase = string.toLowerCase();
+
+//     let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+//     let count = 0;
+
+//     for(let char of lowerCase){
+//         if(vowels.includes(char)){
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// let result = getString("anay");
+// console.log(result);
